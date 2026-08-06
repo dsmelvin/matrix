700d87a97e7177ea27282cdcca76a4fd25d59183
Melvin Wu <dsmelvin@hotmail.com>
2026-08-06 09:55:24 -0700

    update run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "$(realpath "$0")")" && pwd)"
 export BASEDIR="$(dirname "$SCRIPT_DIR")"
+WORKSPACE=$(pwd)
 set -a
 if [ -f "$1" ]; then
     . $1
 else 
     . .env
 fi
+if [ -d "$2" ]; then
+    WORKSPACE=$(realpath "$2")
+fi
 cd $(pwd)
 set +a
 if [ "$1" == "help" ]; then
   MVN_RUN="help operator"
-  mvn -f $BASEDIR/operator/pom.xml spring-boot:run -Dspring-boot.run.workingDirectory=$(pwd) -Dspring-boot.run.arguments="$MVN_RUN"
+  mvn -f $BASEDIR/operator/pom.xml spring-boot:run -Dspring-boot.run.workingDirectory=$WORKSPACE -Dspring-boot.run.arguments="$MVN_RUN"
 else
   MVN_RUN="operator ${@:1}"
-  mvn -f $BASEDIR/operator/pom.xml spring-boot:run -Dspring-boot.run.workingDirectory=$(pwd) -Dspring-boot.run.arguments="$MVN_RUN"
+  mvn -f $BASEDIR/operator/pom.xml spring-boot:run -Dspring-boot.run.workingDirectory=$WORKSPACE -Dspring-boot.run.arguments="$MVN_RUN"
 fi
